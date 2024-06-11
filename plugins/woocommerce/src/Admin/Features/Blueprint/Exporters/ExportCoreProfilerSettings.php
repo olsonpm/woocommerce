495db81f6a2b548d2206c0aad20b6bc3e6a2db8b
moon <moon.kyong@automattic.com>
2024-06-10 17:16:42 -0700

    Fix bug with coreprofiler setting

--- plugins/woocommerce/src/Admin/Features/Blueprint/Exporters/ExportCoreProfilerSettings.php
+++ plugins/woocommerce/src/Admin/Features/Blueprint/Exporters/ExportCoreProfilerSettings.php
@@ -4,21 +4,16 @@ namespace Automattic\WooCommerce\Admin\Features\Blueprint\Exporters;
 
 class ExportCoreProfilerSettings implements ExportsStepSchema {
 	public function export() {
-		$onboarding_profile = get_option('woocommerce_onboarding_profile', array());
 	    return array(
-			'blogname' => get_option('blogname'),
-		    "woocommerce_allow_tracking"=> true,
-			"industry"=> $onboarding_profile['industry'] ?? array(),
-			"business_choice"=> $onboarding_profile['business_choice'] ?? '',
-			"store_email"=> $onboarding_profile['store_email'] ?? '',
-		    'completed' => $onboarding_profile['completed'] ?? false,
-		    'skipped' => $onboarding_profile['skipped'] ?? false,
+		    'blogname' => get_option('blogname'),
+		    "woocommerce_allow_tracking"=> get_option('woocommerce_allow_tracking', false),
+		    'woocommerce_onboarding_profile' => get_option('woocommerce_onboarding_profile', array())
 	    );
 	}
 	public function export_step_schema() {
 		return array(
 			'step' => $this->get_step_name(),
-			'values' => $this->export()
+			'options' => $this->export()
 		);
 	}
 
--- plugins/woocommerce/src/Admin/Features/Blueprint/StepProcessors/ConfigureCoreProfiler.php
+++ plugins/woocommerce/src/Admin/Features/Blueprint/StepProcessors/ConfigureCoreProfiler.php
@@ -9,24 +9,8 @@ use WC_Tax;
 
 class ConfigureCoreProfiler extends SetOptions {
 	public function process($schema): StepProcessorResult {
-		$options = array();
-		foreach (array('blogname', 'woocommerce_allow_tracking') as $standalone_option) {
-			if ( isset( $schema->values->{$standalone_option} ) ) {
-				$options[ $standalone_option ] = $schema->values->{$standalone_option};
-			}
-		}
-
-		// Update onboarding profile values.
-		$onboarding_profile = get_option('woocommerce_onboarding_profile', array());
-		foreach (array('industry', 'business_choice', 'store_email', 'completed', 'skipped') as $profile_item_key) {
-			if (isset($schema->values->{$profile_item_key})) {
-				$onboarding_profile[$profile_item_key] = $schema->values->{$profile_item_key};
-			}
-		}
-
-		$options['woocommerce_onboarding_profile'] = $onboarding_profile;
 		$result = parent::process((object)array(
-			'options' => $options
+			'options' => $schema->options
 		));
 
 		$result->set_step_name('ConfigureCoreProfiler');
